ad01a04015a3ba27a6f51eb888e12a7e83818ff5
Paul Grau <paul@blocko.io>
2018-11-13 17:22:34 +0900

    [backend] parallelize stats requests

--- backend/src/app.js
+++ backend/src/app.js
@@ -30,25 +30,32 @@ app.get('/accountTransactions', async (req, res) => {
 });
 
 app.get('/tx', async (req, res) => {
-    const maxTps = await searchBlock({
-        body: {
-            size: 1,
-            sort: {
-                txs: { "order" : "desc" }
-            },
-        }
-    }, true);
-
-    const bestBlock = await getBestBlock();
-    
-    const txPerSecond = await aggregateBlocks({ gte: "now-60s/s", lt: "now" }, "1s");
-    const txPerMinute = await aggregateBlocks({ gte: "now-60m/m", lt: "now" }, "1m");
-    const txPerHour = await aggregateBlocks({ gte: "now-24h/h", lt: "now" }, "1h");
-    const txPerDay = await aggregateBlocks({ gte: "now-30d/d", lt: "now" }, "1d");
+    const [
+        txPerSecond,
+        txPerMinute,
+        txPerHour,
+        txPerDay,
+        maxTps,
+        bestBlock,
+        blockCount
+    ] = await Promise.all([
+        aggregateBlocks({ gte: "now-60s/s", lt: "now" }, "1s"),
+        aggregateBlocks({ gte: "now-60m/m", lt: "now" }, "1m"),
+        aggregateBlocks({ gte: "now-24h/h", lt: "now" }, "1h"),
+        aggregateBlocks({ gte: "now-30d/d", lt: "now" }, "1d"),
+        searchBlock({
+            body: {
+                size: 1,
+                sort: {
+                    txs: { "order" : "desc" }
+                },
+            }
+        }, true),
+        getBestBlock(),
+        getBlockCount()
+    ]);
 
     const txTotal = txPerDay.map(day => day.sum_txs.value).reduce((a, b) => a + b, 0);
-    
-    const blockCount = await getBlockCount();
     
     return res.json({
         blockCount,
