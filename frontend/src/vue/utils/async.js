72529220556fb1d41fde0afcfae7836e2df012bc
Paul Grau <paul@blocko.io>
2018-11-13 17:38:22 +0900

    Typos in function doc

--- frontend/src/vue/utils/async.js
+++ frontend/src/vue/utils/async.js
@@ -7,14 +7,20 @@ const clamp = function(num, min, max) {
 
 /**
  * A UX hack to wait a minimum amount of time when API responds very quickly.
- * Also good for testing, as production load times are usually faster than
+ * Also good for testing, as production load times are usually slower than
  * in a development environment.
+ * 
+ * Example:
+ *     const waitMinimum = loadAndWait();
+ *     (... do something that the user expects to take some time, e.g. loading data ...)
+ *     await waitMinimum();
+ * 
  * @return {loadAndWait~waitMinimum}
  */
 function loadAndWait(minimumLoadTime = MINIMUM_LOAD_TIME) {
     const startedLoading = + new Date();
     /**
-     * @return {Promise} promise that resolves when minimumLoadTime minus wait time has passed (possible immediately).
+     * @return {Promise} promise that resolves when minimumLoadTime minus wait time has passed (possibly immediately).
      */
     return function waitMinimum() {
         return new Promise(resolve => {
