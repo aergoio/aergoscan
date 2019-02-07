457320f8a02d56d831c7eae72bbe43d0c55ca728
Paul Grau <paul@blocko.io>
2019-02-07 14:04:10 +0900

    fix prod config

--- indexer/entry.sh
+++ indexer/entry.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 reindex=${AERGOSCAN_REINDEX:-false}
 if [ "$reindex" = true ] ; then
-    esindexer -A ${AERGO_NODE} -E ${ES_URL} --reindex
+    esindexer -A ${AERGO_NODE} -E ${ES_URL} --prefix testnet_ --reindex 
 else
-    esindexer -A ${AERGO_NODE} -E ${ES_URL}
+    esindexer -A ${AERGO_NODE} -E ${ES_URL} --prefix testnet_
 fi
