01f7fb22cea6a3cbe03462b7113156877159a99d
Paul Grau <paul@blocko.io>
2019-03-28 17:43:03 +0900

    change fee unit

--- frontend/src/vue/pages/Transaction.vue
+++ frontend/src/vue/pages/Transaction.vue
@@ -21,7 +21,7 @@
             
             <table class="detail-table">
               <tr><td>Amount:</td><td v-html="$options.filters.formatToken(txDetail.tx.amount, 'aergo')"></td></tr>
-              <tr v-if="txReceipt"><td>Fee:</td><td v-html="$options.filters.formatToken(this.txReceipt.fee, 'gaer')"></td></tr>
+              <tr v-if="txReceipt"><td>Fee:</td><td v-html="$options.filters.formatToken(this.txReceipt.fee, 'aergo')"></td></tr>
               <tr><td>Nonce:</td><td>{{txDetail.tx.nonce}}</td></tr>
               <tr v-if="txDetail.block">
                 <td>Included in block:</td>
