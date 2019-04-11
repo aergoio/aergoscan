cce3d7f21daacaef3a123c14e774ad9b3d7f1c41
Paul Grau <paul@blocko.io>
2019-04-11 11:26:27 +0900

    add mainnet to backend

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,6 +1,6 @@
 export default {
     HTTP_PORT: 3000,
     DB_HOST: process.env.ES_HOST || 'db:9200',
-    AVAILABLE_NETWORKS: ['testnet', 'chain'], // chain = testnet, may be renamed later
+    AVAILABLE_NETWORKS: ['testnet', 'chain', 'main'], // chain = testnet, may be renamed later
     HOST: 'https://api.aergoscan.io'
 };
