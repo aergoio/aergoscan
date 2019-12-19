e947823d1d27a370ece9a89dd3594474ffbd0e53
Paul Grau <paul@blocko.io>
2019-12-19 16:17:35 +0900

    Revert 5d8b911

--- frontend/src/controller.js
+++ frontend/src/controller.js
@@ -1,17 +1,6 @@
 import Aergo, { GrpcWebProvider } from '@herajs/client';
 import cfg from './config.js';
-
-function getNodeUrl() {
-    try {
-        // Allow to override URL manually
-        const urlParams = new URLSearchParams(window.location.search);
-        return urlParams.get('override_node') || cfg.AERGO_URL;
-    } catch (e) {
-        return cfg.AERGO_URL;
-    }
-}
-
-const provider = new GrpcWebProvider({url: getNodeUrl()});
+const provider = new GrpcWebProvider({url: cfg.AERGO_URL});
 const aergo = new Aergo({}, provider);
 
 export default aergo;
