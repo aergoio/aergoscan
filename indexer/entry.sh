a56d9f4ecfd583a128d886bae36789ca6dafe914
Paul Grau <paul@blocko.io>
2020-03-27 18:23:29 +0900

    Update docker-compose setup to use new indexer

--- indexer/entry.sh
+++ indexer/entry.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 reindex=${AERGOSCAN_REINDEX:-false}
 if [ "$reindex" = true ] ; then
-    esindexer -A ${AERGO_NODE} -E ${ES_URL} --prefix testnet_ --reindex 
+    indexer -A ${AERGO_NODE} -E ${ES_URL} --prefix testnet_ --reindex 
 else
-    esindexer -A ${AERGO_NODE} -E ${ES_URL} --prefix testnet_
+    indexer -A ${AERGO_NODE} -E ${ES_URL} --prefix testnet_
 fi
