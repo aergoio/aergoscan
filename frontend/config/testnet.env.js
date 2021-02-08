eaa820afc6bc94be30b0054ee3d9edf5e47e29b7
HoChul Seok <ultrafellen@gmail.com>
2021-02-08 16:51:02 +0900

    split prod and testnet

--- frontend/config/testnet.env.js
+++ frontend/config/testnet.env.js
@@ -7,9 +7,19 @@ if (process.env.BACKEND_URL) {
     }
 }
 
+let AERGO_URL = `https://testnet-api-http.aergo.io`;
+if (process.env.AERGO_URL) {
+    if (process.env.AERGO_URL.startsWith('http')) {
+        AERGO_URL = process.env.AERGO_URL;
+    } else {
+        const aergo_node_clean = process.env.AERGO_URL.replace(/https?:\/\//,'');
+        AERGO_URL = `https://${aergo_node_clean}/aergo`;
+    }
+}
+
 module.exports = {
     DEPLOYMENT: JSON.stringify('testnet'),
     NODE_ENV: JSON.stringify('production'),
     API_URL: JSON.stringify(`${BACKEND_URL}/testnet`),
-    AERGO_URL: JSON.stringify(`${BACKEND_URL}/aergo`),
+    AERGO_URL: JSON.stringify(`${AERGO_URL}`),
 }
